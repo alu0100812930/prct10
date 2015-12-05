7f8acdf1fa3b3c721a4467761f56cfa36c279a8a
Mauricio José Orta Rodríguez <alu0100812930@ull.edu.es>
2015-12-05 01:08:25 +0000

    Desarrollo de código de clase de lista doblemente enlazada, clase hija de lista de referencias y prueba exitosa de expectativa de orden alfabético

---
+++ lib/functional/ref_list.rb
@@ -0,0 +1,15 @@
+class RList < DList
+  def initialize(content = nil)
+  super
+  ordenar
+  end
+  
+  def ordenar
+     orden = sort_by{|node| node["value"]}
+     extract_all
+     for i in 0..orden.count-1
+     insert_e(orden[i]["value"])
+ end
+ end
+    
+end
